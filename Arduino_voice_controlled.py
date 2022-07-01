41116051149f2ebc404647d77b067e24295664c5
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-07-01 19:21:17 +0530

    Uncommented some test lines

--- Arduino_voice_controlled.py
+++ Arduino_voice_controlled.py
@@ -30,29 +30,29 @@ if __name__ == "__main__":
         query = takeCommand().lower()
         query=set(query)
         if 'on' in query:
-            '''if 'lamp' in query:
+            if 'lamp' in query:
                 board.digital[12].write(1)
             if 'fan' in query:
                 board.digital[11].write(1)
             if 'bulb' in query:
                 board.digital[10].write(1)
             if 'night' in query:
-                board.digital[9].write(1)'''
+                board.digital[9].write(1)
             board.digital[2].write(1)
         elif 'off' in query:
-            '''if 'lamp' in query:
+            if 'lamp' in query:
                 board.digital[12].write(0)
             if 'fan' in query:
                 board.digital[11].write(0)
             if 'bulb' in query:
                 board.digital[10].write(0)
             if 'night' in query:
-                board.digital[9].write(0)'''
+                board.digital[9].write(0)
             board.digital[2].write(0)
         elif 'exit' in query:
-            '''board.digital[9].write(0)
+            board.digital[9].write(0)
             board.digital[10].write(0)
             board.digital[11].write(0)
-            board.digital[12].write(0)'''
+            board.digital[12].write(0)
             board.digital[2].write(2)
             exit()
